3304d68a2885818e2fa84d22612f235b68c2e82c
Jeremy Lenz <jeremylenz@gmail.com>
2017-05-27 18:28:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,37 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+require 'pry'
+pigeon_list = {}
+pigeon_names = []
+
+data.each do |attribute, value|  # Scrape up all the pigeon names
+  value.each do |value, pigeons|
+    pigeons.each do |name|
+      pigeon_names << name
+    end
+  end
+end
+
+pigeon_names.uniq!  #Clean up the pigeon names
+
+# binding.pry
+
+pigeon_names.each do |pigeon_name|  #Add the pigeon names and blank values
+  pigeon_list[pigeon_name]= {color: [], gender: [], lives: []}
+end
+
+# binding.pry
+
+
+data.each do |attribute, value|
+  value.each do |value_2, pigeons|
+    pigeons.each do |pigeon_name|
+      # binding.pry
+      pigeon_list[pigeon_name][attribute] << value_2.to_s
+    end
+  end
+end
+
+# binding.pry
+
+pigeon_list
 end
